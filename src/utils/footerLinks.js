d06dfbec3cb5bdf199d28cfaf6071545019d30f1
Henrique Macedo <mail@henriquemacedo.pt>
2019-01-21 11:27:17 +0000

    Added DEV Community profile in footer links

--- src/utils/footerLinks.js
+++ src/utils/footerLinks.js
@@ -1,14 +1,15 @@
 const footerLinks = [
-  { key: 2, link: 'https://github.com/henriquemacedo', name: 'GitHub' },
-  { key: 1, link: 'https://dribbble.com/henriquemacedo', name: 'Dribbble' },
-  { key: 3, link: 'https://twitter.com/henrikemacedo', name: 'Twitter' },
-  { key: 4, link: 'https://instagram.com/henrikemacedo/', name: 'Instagram' },
+  { key: 1, link: 'https://github.com/henriquemacedo', name: 'GitHub' },
+  { key: 2, link: 'https://dev.to/henrique', name: 'DEV Community' },
+  { key: 3, link: 'https://dribbble.com/henriquemacedo', name: 'Dribbble' },
+  { key: 4, link: 'https://twitter.com/henrikemacedo', name: 'Twitter' },
+  { key: 5, link: 'https://instagram.com/henrikemacedo/', name: 'Instagram' },
   {
-    key: 5,
+    key: 6,
     link: 'https://www.linkedin.com/in/henrikemacedo',
     name: 'Linkedin',
   },
-  { key: 6, link: 'mailto:mail@henriquemacedo.pt', name: 'Email' },
+  { key: 7, link: 'mailto:mail@henriquemacedo.pt', name: 'Email' },
 ]
 
 export default footerLinks
