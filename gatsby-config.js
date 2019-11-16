5229bf52106ccac25ba38ef6e1c3d2ba1b83ff1e
Henrique Macedo <mail@henriquemacedo.pt>
2019-11-16 23:14:27 +0000

    Add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: 'https://henriquemacedo.pt',
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-robots-txt',
