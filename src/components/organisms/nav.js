3d1a1d538962bb80a34c2394f70e10f823fcf14f
Henrique Macedo <mail@henriquemacedo.pt>
2020-02-12 21:44:19 +0000

    Update navigation bar

--- src/components/organisms/nav.js
+++ src/components/organisms/nav.js
@@ -5,37 +5,28 @@ import { Link } from 'gatsby'
 import { device } from '../ions/breakpoints'
 
 const Wrapper = styled.nav`
-  border: 1px solid red;
   position: fixed;
-  margin-top: 2vw;
   width: 100%;
-  padding: 0 2vw;
-  font-size: 0.75rem;
-  font-weight: var(--bold, 700);
+  padding: 5vw;
+  font-size: 12px;
+  letter-spacing: 3px;
   text-transform: uppercase;
   z-index: 1;
 
-  /* @media ${device.s} {
-    font-size: 0.5rem;
-  } */
+  @media ${device.s} {
+    padding: 2vw;
+  }
 
   ul {
     margin: 0;
     padding: 0;
     display: flex;
-    /* flex-direction: row; */
     justify-content: space-between;
 
     li {
-      border: 1px solid yellow;
       list-style: none;
 
-      /* &:not(:last-child) {
-        margin-right: 40px;
-      } */
-
       a {
-        /* position: relative; */
         color: var(--white, #ededed);
         text-decoration: none;
         -webkit-transition: 0.2s;
@@ -44,20 +35,8 @@ const Wrapper = styled.nav`
         transition: 0.2s;
 
         &:hover {
-          color: var(--highlight, #e0be24)
+          color: var(--highlight, #e0be24);
         }
-
-        /* &.active {
-          &:after {
-            content: '';
-            position: absolute;
-            left: 5px;
-            bottom: -5px;
-            background-color: var(--highlight, #e0be24);
-            width: 100%;
-            height: 5px;
-          }
-        } */
       }
     }
   }
